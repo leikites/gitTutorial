3752e58ffb06e686d2a7f738ba582283923ce3ec
leikites <xhe97@uwo.ca>
2017-02-15 14:08:12 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by hexiaolei on 2017/2/15.
+ */
+public class Course {
+    String cname;
+    String desctiption;
+
+    public Course(String cname,String description){
+        this.cname = cname;
+        this.desctiption = description;
+    }
+}
